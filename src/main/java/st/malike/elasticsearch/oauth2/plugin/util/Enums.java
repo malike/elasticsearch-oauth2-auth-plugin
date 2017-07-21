909f8285e766a67b591568eb487f521399ce8791
malike <st.malike@gmail.com>
2017-07-21 22:09:59 +0000

    initial setup for elasticsearch authentication plugin

---
+++ src/main/java/st/malike/elasticsearch/oauth2/plugin/util/Enums.java
@@ -0,0 +1,17 @@
+package st.malike.elasticsearch.oauth2.plugin.util;
+
+/**
+ * @author malike_st
+ */
+public class Enums {
+
+    public enum JSONResponseMessage {
+        SUCCESS,
+        MISSING_PARAM,
+        NOT_AUTHORISED,
+        ERROR
+    }
+
+
+
+}
